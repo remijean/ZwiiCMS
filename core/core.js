ee778a9ffac262fd5f6492402626281a03273844
Rémi Jean <remi.jean@outlook.com>
2016-03-16 23:51:54 +0100

    Intégration de TinyMCE fix #92

--- core/core.js
+++ core/core.js
@@ -73,29 +73,99 @@ $('#module').on('change', function() {
 	}
 });
 
-/* Charge l'éditeur de texte */
-$.extend(true, $.trumbowyg.upload, {
-	serverPath: baseUrl + 'upload',
-	fileFieldName: 'file',
-	urlPropertyName: 'link'
-});
-$('.editor').trumbowyg({
-	btnsDef: {
-		image: {
-			dropdown: ['insertImage', 'upload'],
-			ico: 'insertImage'
-		}
+/* Filtre la langue du site au format de tinyMCE */
+if(!language) {
+	language = 'fr_FR';
+}
+else if(language === 'en_EN.json') {
+	language = '';
+}
+else {
+	language = language.substring(0, language.length - 5);
+}
+
+/* Charge tinyMCE */
+tinymce.init({
+	selector: '.editor',
+	language: language,
+	plugins: 'advlist anchor autolink autoresize charmap code fullscreen hr image imagetools legacyoutput link lists media nonbreaking noneditable paste preview print searchreplace tabfocus table textpattern wordcount',
+	toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media',
+	body_class: bodyClass + ' editor',
+	content_css: [
+		'core/theme.css',
+		'core/custom.css'
+	],
+	image_advtab: true,
+	file_browser_callback: function(fieldName) {
+		$('#editorField').val(fieldName);
+		$('#editorFile').trigger('click');
 	},
-	btns: [
-		'viewHTML',
-		'|', 'formatting',
-		'|', 'btnGrp-design',
-		'|', 'link',
-		'|', 'image',
-		'|', 'btnGrp-justify',
-		'|', 'btnGrp-lists',
-		'|', 'horizontalRule'
-	]
+	file_browser_callback_types: 'image'
+});
+
+/* Ajoute le formulaire d'upload de tinyMCE */
+if($('.editor').length && !$('#editorFileForm').length) {
+	$('body').append(
+		$('<form>').attr({
+			id: 'editorForm',
+			enctype: 'multipart/form-data',
+			method: 'post'
+		}).append(
+			$('<input>').addClass('hide').attr({
+				id: 'editorFile',
+				type: 'file'
+			}),
+			$('<input>').attr({
+				id: 'editorField',
+				type: 'hidden'
+			})
+		)
+	);
+}
+
+/* Upload de tinyMCE */
+$('#editorFile').on('change', function() {
+	var editorField = $('#editorField').val();
+	var editorFieldShort = editorField.substring(0, editorField.length - 3);
+	// Affiche le statut de l'upload
+	function uploadStatus(color) {
+		$('#' + editorField).val('').css('border-color', color);
+		$('#' + editorFieldShort + 'l').css('color', color);
+		$('#' + editorFieldShort + 'action').css({
+			backgroundColor: color,
+			borderColor: color
+		});
+	}
+	// Upload d'image
+	var file = this.files[0];
+	if(file !== undefined && file.type.substring(0, 5) === 'image') {
+		var formData = new FormData();
+		formData.append('file', file);
+		$.ajax({
+			type: 'POST',
+			url: baseUrl + 'upload',
+			data: formData,
+			dataType: 'json',
+			cache: false,
+			contentType: false,
+			processData: false,
+			success: function(data) {
+				if(data.error) {
+					uploadStatus('#E74C3C');
+				}
+				else {
+					uploadStatus('#1ABC9C');
+					$('#' + editorField).val(data.link);
+				}
+			},
+			error: function() {
+				uploadStatus('#E74C3C');
+			}
+		});
+	}
+	else {
+		uploadStatus('#E74C3C');
+	}
 });
 
 /* Aperçu de la personnalisation en direct */
