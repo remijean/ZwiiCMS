7339cb0ccf97f65c125ceed2b2dadfea83821a45
Rémi Jean <remi.jean@outlook.com>
2016-04-17 17:03:28 +0200

    Amélioration du menu transparent

--- core/core.php
+++ core/core.php
@@ -768,8 +768,8 @@ class core
 			}
 			// Couleur de survole des liens et couleur de fond blanche pour les sous menu si le menu est transparent
 			else {
-				$colorDark = '0,0,0,.1';
-				$colorVeryDark = '0,0,0,.2';
+				$colorDark = '0,0,0,.05';
+				$colorVeryDark = '0,0,0,.1';
 				$css .= '
 					/* Couleur blanche des sous menus */
 					nav li ul {
@@ -2415,8 +2415,8 @@ class core
 						// Transparence
 						else {
 							color = "0,0,0,0";
-							colorDark = "0,0,0,.1";
-							colorVeryDark = "0,0,0,.2";
+							colorDark = "0,0,0,.05";
+							colorVeryDark = "0,0,0,.1";
 							textVariant = "inherit";
 							// Sous menu blanc car le menu est transparent
 							colorMenuChildren = "255,255,255,1";
