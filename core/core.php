afb342e4e1c36c09ecdf168401c77a1a68c46b8b
Rémi Jean <remi.jean@outlook.com>
2016-04-24 16:23:54 +0200

    Ajout du code Google Analytics pour l'administrateur

--- core/core.php
+++ core/core.php
@@ -987,9 +987,7 @@ class core extends common
 	 */
 	public function showAnalytics()
 	{
-		$code = $this->getData(['config', 'analytics']);
-		// Check si ce n'est pas l'administrateur
-		if(!$this->getCookie() AND !empty($code) ) {
+		if($code = $this->getData(['config', 'analytics'])) {
 			$ga = '(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){';
 			$ga .= '(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),';
 			$ga .= 'm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)';
