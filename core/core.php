1569dbd26ae612763756af0a8e6a4612da7a0ce8
Rémi Jean <remi.jean@outlook.com>
2016-04-25 18:23:58 +0200

    Ajout de l'insertion de balises script depuis TinyMCE

--- core/core.php
+++ core/core.php
@@ -4182,6 +4182,7 @@ class template
 					plugins: "advlist anchor autolink autoresize charmap code colorpicker contextmenu fullscreen hr image imagetools legacyoutput link lists media nonbreaking noneditable paste preview print searchreplace tabfocus table textcolor textpattern visualchars wordcount",
 					toolbar: "insertfile undo redo | styleselect | bold italic forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
 					body_class: "editor",
+					extended_valid_elements: "script[language|type|src]",
 					content_css: [
 						"core/theme.css",
 						"core/cache/' . core::$cssVersion . '.css",
