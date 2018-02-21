6d06b21b469b9d019bcf48da6254101ac4e853bd
Rémi Jean <remi.jean@outlook.com>
2018-02-21 13:59:07 +0100

    Mise à jour de Flatpickr en 4.3.2

--- core/vendor/flatpickr/l10n/fr.js
+++ core/vendor/flatpickr/l10n/fr.js
@@ -1,25 +1,75 @@
-/* French locals for flatpickr */
-var Flatpickr = Flatpickr || { l10ns: {} };
-Flatpickr.l10ns.fr = {};
+/* flatpickr v4.3.2, @license MIT */
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+		typeof define === 'function' && define.amd ? define(['exports'], factory) :
+			(factory((global.fr = {})));
+}(this, (function (exports) { 'use strict';
 
-Flatpickr.l10ns.fr.firstDayOfWeek = 1;
+	var fp = typeof window !== "undefined" && window.flatpickr !== undefined
+		? window.flatpickr
+		: {
+			l10ns: {},
+		};
+	var French = {
+		firstDayOfWeek: 1,
+		weekdays: {
+			shorthand: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
+			longhand: [
+				"Dimanche",
+				"Lundi",
+				"Mardi",
+				"Mercredi",
+				"Jeudi",
+				"Vendredi",
+				"Samedi",
+			],
+		},
+		months: {
+			shorthand: [
+				"Janv",
+				"Févr",
+				"Mars",
+				"Avr",
+				"Mai",
+				"Juin",
+				"Juil",
+				"Août",
+				"Sept",
+				"Oct",
+				"Nov",
+				"Déc",
+			],
+			longhand: [
+				"Janvier",
+				"Février",
+				"Mars",
+				"Avril",
+				"Mai",
+				"Juin",
+				"Juillet",
+				"Août",
+				"Septembre",
+				"Octobre",
+				"Novembre",
+				"Décembre",
+			],
+		},
+		ordinal: function (nth) {
+			if (nth > 1)
+				return "ème";
+			return "er";
+		},
+		rangeSeparator: " au ",
+		weekAbbreviation: "Sem",
+		scrollTitle: "Défiler pour augmenter la valeur",
+		toggleTitle: "Cliquer pour basculer",
+	};
+	fp.l10ns.fr = French;
+	var fr = fp.l10ns;
 
-Flatpickr.l10ns.fr.weekdays = {
-	shorthand: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
-	longhand: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi']
-};
+	exports.French = French;
+	exports.default = fr;
 
-Flatpickr.l10ns.fr.months = {
-	shorthand: ['Janv', 'Févr', 'Mars', 'Avr', 'Mai', 'Juin', 'Juil', 'Août', 'Sept', 'Oct', 'Nov', 'Déc'],
-	longhand: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
-};
+	Object.defineProperty(exports, '__esModule', { value: true });
 
-Flatpickr.l10ns.fr.ordinal = function (nth) {
-	if (nth > 1) {
-		return "ème";
-	}
-	return "er";
-};
-if (typeof module !== "undefined") {
-	module.exports = Flatpickr.l10ns;
-}
+})));
