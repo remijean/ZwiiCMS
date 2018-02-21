83e9ab1eb4356e4caa45216fd3a6be5affc26afe
Rémi Jean <remi.jean@outlook.com>
2018-02-21 23:24:57 +0100

    Mise à jour de TinyMCE en 4.7.7 et ajout du plugin template afin de créer des colonnes adaptatives

--- core/vendor/tinymce/init.js
+++ core/vendor/tinymce/init.js
@@ -3,14 +3,16 @@
  */
 tinymce.init({
 	selector: ".editorWysiwyg",
-	language: language,
-	plugins: "advlist anchor autolink autoresize code colorpicker contextmenu fullscreen hr image imagetools legacyoutput link lists media paste preview searchreplace tabfocus table textcolor textpattern wordcount",
-	toolbar: "insertfile undo redo | styleselect | bold italic forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+	language: "fr_FR",
+	plugins: "advlist anchor autolink autoresize code colorpicker contextmenu fullscreen hr image imagetools legacyoutput link lists media paste preview searchreplace tabfocus table template textcolor textpattern wordcount",
+	toolbar: "insertfile undo redo | styleselect | bold italic forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media template table",
 	body_class: "editorWysiwyg",
 	extended_valid_elements: "script[language|type|src]",
 	content_css: [
 		baseUrl + "core/layout/common.css",
-		baseUrl + "site/data/theme.css"
+		baseUrl + "core/vendor/tinymce/init.css",
+		baseUrl + "site/data/theme.css",
+		baseUrl + "site/data/custom.css"
 	],
 	relative_urls: false,
 	document_base_url: baseUrl,
@@ -19,6 +21,24 @@ tinymce.init({
 	external_plugins: {
 		"filemanager": baseUrl + "core/vendor/filemanager/plugin.min.js"
 	},
+	// Templates
+	templates: [
+		{
+			title: "Deux colonnes",
+			url: baseUrl + "core/vendor/tinymce/templates/col6.html",
+			description: "Grille de deux colonnes adaptatives, en mode mobile elles passent les unes en dessous des autres."
+		},
+		{
+			title: "Trois colonnes",
+			url: baseUrl + "core/vendor/tinymce/templates/col4.html",
+			description: "Grille de trois colonnes adaptatives, en mode mobile elles passent les unes en dessous des autres."
+		},
+		{
+			title: "Quatre colonnes",
+			url: baseUrl + "core/vendor/tinymce/templates/col3.html",
+			description: "Grille de quatre colonnes adaptatives, en mode mobile elles passent les unes en dessous des autres."
+		}
+	],
 	// Permet de détecter un changement dans la textearea, utile pour le message des modifications non enregistrées du formulaire
 	setup: function(editor) {
 		editor.on("keydown", function() {
