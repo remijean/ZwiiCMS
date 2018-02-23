13f3079943637365958b97fa8f554e68f02c85a3
Rémi Jean <remi.jean@outlook.com>
2018-02-23 10:51:04 +0100

    Correction du bouton mode avancé qui disparaissait lorsqu'une zone du site était cachée

--- core/module/theme/view/index/index.php
+++ core/module/theme/view/index/index.php
@@ -5,7 +5,7 @@
 ): ?>
 	<?php echo template::speech('Cliquez sur une zone afin d\'accéder à ses options de personnalisation. Vous pouvez également afficher les zones cachées à l\'aide du bouton ci-dessous.'); ?>
 	<div class="row">
-		<div class="col2 offset4">
+		<div class="col2 offset3">
 			<?php echo template::button('themeBack', [
 				'class' => 'buttonGrey',
 				'href' => helper::baseUrl(false),
@@ -17,6 +17,12 @@
 			<?php echo template::button('themeShowAll', [
 				'ico' => 'eye',
 				'value' => 'Zones'
+			]); ?>
+		</div>
+		<div class="col2">
+			<?php echo template::button('themeAdvanced', [
+				'href' => helper::baseUrl() . $this->getUrl(0) . '/advanced',
+				'value' => 'Mode avancé'
 			]); ?>
 		</div>
 	</div>
