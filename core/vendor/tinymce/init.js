740129b7c0cb69aecb704c13cd2c41bf9638937e
Rémi Jean <remi.jean@outlook.com>
2018-02-26 13:52:52 +0100

    Ajout d'une surcouche graphique à TinyMCE + le plugin de récupération des données

--- core/vendor/tinymce/init.js
+++ core/vendor/tinymce/init.js
@@ -4,9 +4,41 @@
 tinymce.init({
 	selector: ".editorWysiwyg",
 	language: "fr_FR",
-	plugins: "advlist anchor autolink autoresize code colorpicker contextmenu fullscreen hr image imagetools legacyoutput link lists media paste preview searchreplace tabfocus table template textcolor textpattern wordcount",
-	toolbar: "insertfile undo redo | styleselect | bold italic forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media template",
+	menubar: false,
+	statusbar: false,
+	plugins: "advlist anchor autolink autoresize autosave code colorpicker contextmenu fullscreen hr image imagetools legacyoutput link lists media paste searchreplace tabfocus table template textcolor",
+	toolbar: "restoredraft | undo redo | styleselect | bold italic forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist | insert | code searchreplace fullscreen",
+	insert_button_items: 'image link media template inserttable | hr | anchor',
 	body_class: "editorWysiwyg",
+	style_formats: [
+		{title: 'Headers', items: [
+			{title: 'Header 1', format: 'h1'},
+			{title: 'Header 2', format: 'h2'},
+			{title: 'Header 3', format: 'h3'},
+			{title: 'Header 4', format: 'h4'}
+		]},
+		{title: 'Inline', items: [
+			{title: 'Bold', icon: 'bold', format: 'bold'},
+			{title: 'Italic', icon: 'italic', format: 'italic'},
+			{title: 'Underline', icon: 'underline', format: 'underline'},
+			{title: 'Strikethrough', icon: 'strikethrough', format: 'strikethrough'},
+			{title: 'Superscript', icon: 'superscript', format: 'superscript'},
+			{title: 'Subscript', icon: 'subscript', format: 'subscript'},
+			{title: 'Code', icon: 'code', format: 'code'}
+		]},
+		{title: 'Blocks', items: [
+			{title: 'Paragraph', format: 'p'},
+			{title: 'Blockquote', format: 'blockquote'},
+			{title: 'Div', format: 'div'},
+			{title: 'Pre', format: 'pre'}
+		]},
+		{title: 'Alignment', items: [
+			{title: 'Left', icon: 'alignleft', format: 'alignleft'},
+			{title: 'Center', icon: 'aligncenter', format: 'aligncenter'},
+			{title: 'Right', icon: 'alignright', format: 'alignright'},
+			{title: 'Justify', icon: 'alignjustify', format: 'alignjustify'}
+		]}
+	],
 	extended_valid_elements: "script[language|type|src]",
 	content_css: [
 		baseUrl + "core/layout/common.css",
@@ -65,11 +97,5 @@ tinymce.init({
 			url: baseUrl + "core/vendor/tinymce/templates/col10-2.html",
 			description: "Grille adaptative sur 12 colonnes, sur mobile elles passent les unes en dessous des autres."
 		}
-	],
-	// Permet de détecter un changement dans la textearea, utile pour le message des modifications non enregistrées du formulaire
-	setup: function(editor) {
-		editor.on("keydown", function() {
-			$(editor.targetElm).trigger("keydown");
-		});
-	}
+	]
 });
