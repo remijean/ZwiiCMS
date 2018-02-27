51bd24d2afda0facab95d78cf44efee502b21eaf
Rémi Jean <remi.jean@outlook.com>
2018-02-27 21:05:18 +0100

    Mise à jour de TinyMCE en 4.7.8 et ajout d'un mode mobile

--- core/vendor/tinymce/init.js
+++ core/vendor/tinymce/init.js
@@ -2,59 +2,79 @@
  * Initialisation de TinyMCE
  */
 tinymce.init({
+	// Classe où appliquer l'éditeur
 	selector: ".editorWysiwyg",
+	// Langue
 	language: "fr_FR",
-	menubar: false,
-	statusbar: false,
+	// Plugins
 	plugins: "advlist anchor autolink autoresize autosave code colorpicker contextmenu fullscreen hr image imagetools legacyoutput link lists media paste searchreplace tabfocus table template textcolor",
+	// Contenu de la barre d'outils
 	toolbar: "restoredraft | undo redo | styleselect | bold italic forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist | insert | code searchreplace fullscreen",
-	insert_button_items: 'image link media template inserttable | hr | anchor',
-	body_class: "editorWysiwyg",
-	style_formats: [
-		{title: 'Headers', items: [
-			{title: 'Header 1', format: 'h1'},
-			{title: 'Header 2', format: 'h2'},
-			{title: 'Header 3', format: 'h3'},
-			{title: 'Header 4', format: 'h4'}
-		]},
-		{title: 'Inline', items: [
-			{title: 'Bold', icon: 'bold', format: 'bold'},
-			{title: 'Italic', icon: 'italic', format: 'italic'},
-			{title: 'Underline', icon: 'underline', format: 'underline'},
-			{title: 'Strikethrough', icon: 'strikethrough', format: 'strikethrough'},
-			{title: 'Superscript', icon: 'superscript', format: 'superscript'},
-			{title: 'Subscript', icon: 'subscript', format: 'subscript'},
-			{title: 'Code', icon: 'code', format: 'code'}
-		]},
-		{title: 'Blocks', items: [
-			{title: 'Paragraph', format: 'p'},
-			{title: 'Blockquote', format: 'blockquote'},
-			{title: 'Div', format: 'div'},
-			{title: 'Pre', format: 'pre'}
-		]},
-		{title: 'Alignment', items: [
-			{title: 'Left', icon: 'alignleft', format: 'alignleft'},
-			{title: 'Center', icon: 'aligncenter', format: 'aligncenter'},
-			{title: 'Right', icon: 'alignright', format: 'alignright'},
-			{title: 'Justify', icon: 'alignjustify', format: 'alignjustify'}
-		]}
-	],
-	extended_valid_elements: "script[language|type|src]",
+	// Fichiers CSS à intégrer à l'éditeur
 	content_css: [
 		baseUrl + "core/layout/common.css",
 		baseUrl + "core/vendor/tinymce/content.css",
 		baseUrl + "site/data/theme.css",
 		baseUrl + "site/data/custom.css"
 	],
+	// Classe à ajouter à la balise body dans l'iframe
+	body_class: "editorWysiwyg",
+	// Cache les menus
+	menubar: false,
+	// Cache la barre de statut
+	statusbar: false,
+	// Autorise l'ajout de script
+	extended_valid_elements: "script[language|type|src]",
+	// Bloque le dimensionnement des médias (car automatiquement en fullsize avec fitvids pour le responsive)
 	media_dimensions: false,
+	// Active l'onglet avancé lors de l'ajout d'une image
 	image_advtab: true,
+	// Urls absolues
 	relative_urls: false,
+	// Url de base
 	document_base_url: baseUrl,
+	// Gestionnaire de fichiers
 	filemanager_access_key: privateKey,
 	external_filemanager_path: baseUrl + "core/vendor/filemanager/",
 	external_plugins: {
 		"filemanager": baseUrl + "core/vendor/filemanager/plugin.min.js"
 	},
+	// Thème mobile
+	mobile: {
+		theme: "mobile"
+	},
+	// Contenu du bouton insérer
+	insert_button_items: "image link media template inserttable | hr | anchor",
+	// Contenu du bouton formats
+	style_formats: [
+		{title: "Headers", items: [
+			{title: "Header 1", format: "h1"},
+			{title: "Header 2", format: "h2"},
+			{title: "Header 3", format: "h3"},
+			{title: "Header 4", format: "h4"}
+		]},
+		{title: "Inline", items: [
+			{title: "Bold", icon: "bold", format: "bold"},
+			{title: "Italic", icon: "italic", format: "italic"},
+			{title: "Underline", icon: "underline", format: "underline"},
+			{title: "Strikethrough", icon: "strikethrough", format: "strikethrough"},
+			{title: "Superscript", icon: "superscript", format: "superscript"},
+			{title: "Subscript", icon: "subscript", format: "subscript"},
+			{title: "Code", icon: "code", format: "code"}
+		]},
+		{title: "Blocks", items: [
+			{title: "Paragraph", format: "p"},
+			{title: "Blockquote", format: "blockquote"},
+			{title: "Div", format: "div"},
+			{title: "Pre", format: "pre"}
+		]},
+		{title: "Alignment", items: [
+			{title: "Left", icon: "alignleft", format: "alignleft"},
+			{title: "Center", icon: "aligncenter", format: "aligncenter"},
+			{title: "Right", icon: "alignright", format: "alignright"},
+			{title: "Justify", icon: "alignjustify", format: "alignjustify"}
+		]}
+	],
 	// Templates
 	templates: [
 		{
